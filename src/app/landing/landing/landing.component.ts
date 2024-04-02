9d17efc07956480f9fff8e220be598ffec4a0168
Franklin Godspower Wagbara <wagbarafranklin@yahoo.com>
2024-04-02 16:14:41 +0100

    update

--- src/app/landing/landing/landing.component.ts
+++ src/app/landing/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-landing',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
-export class LandingComponent {}
+export class LandingComponent {
+  public ELPS_URL = environment.ELPS_URL;
+  public APP_PK = environment.APP_PK;
+
+  public onOpenSupport() {}
+}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  ELPS_URL: 'https://elpsdemo.nuprc.gov.ng',
+  API_URL: 'https://localhost:7129/api',
+  APP_PK: '',
+};
